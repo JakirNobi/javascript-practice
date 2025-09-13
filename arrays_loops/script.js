5904ad9b1234445135f049f44279ce77a3cfaa58
JakirNobi <jakirnoby@gmail.com>
2025-09-13 20:38:06 +0600

    Todo list using loop and array

---
+++ arrays_loops/script.js
@@ -0,0 +1,30 @@
+// const myArr = [1, 2, 3];
+// console.log(myArr);
+// myArr[3] = 15;
+// console.log(myArr);
+// myArr[3] = 12;
+// console.log(myArr[3]);
+// const nestArr = [1, 2, 3, "hello", true, { name: "pranto" }, [1, 2]];
+// console.log(nestArr);
+// console.log(typeof nestArr);
+// console.log(nestArr.length);
+// nestArr.push(2);
+// console.log(nestArr);
+// nestArr.splice(0, 3);
+// console.log(nestArr);
+// nestArr.pop();
+// console.log(nestArr);
+
+// let i = 1;
+//
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+// const arr = [];
+//
+// for (let i = 0; i < 10; i++) {
+//   value = i;
+//   arr.push(value);
+//   console.log(arr);
+// }
